a3d07ab0b582119459a3d7b0a852f998710947eb
Tillman Dillon <tillmandillon@icloud.com>
2021-01-31 05:03:51 -0500

    Seeing if the error was putting add_user instead of register_user in the javascript.

--- backend/frontend/javascript.js
+++ backend/frontend/javascript.js
@@ -3,7 +3,7 @@ function SignUp(usernameID, passwordID, emailID) {
 		formData.append('username', document.getElementById(usernameID));
 		formData.append('password', document.getElementById(passwordID));
 		formData.append('email', document.getElementById(emailID));
-		var endpoint = '/add_user';
+		var endpoint = '/register_user';
 		
 		$.ajax({
 			type: 'POST',
